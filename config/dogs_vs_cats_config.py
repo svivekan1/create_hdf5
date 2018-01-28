7c02af8a26739cfdfb7b1681c6102fa46802e12c
svivekan1 <sanjayvivek@hotmail.com>
2018-01-28 11:06:19 +0000

    First commit

--- config/dogs_vs_cats_config.py
+++ config/dogs_vs_cats_config.py
@@ -8,8 +8,7 @@ NUM_CLASSES = 2
 NUM_VAL_IMAGES = 1250 * NUM_CLASSES
 NUM_TEST_IMAGES = 1250 * NUM_CLASSES
 
-# define the path to the output training, validation, and testing
-# HDF5 files
+# define the path to the output training, validation, and testing HDF5 files
 TRAIN_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/train.hdf5"
 VAL_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/val.hdf5"
 TEST_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/test.hdf5"
