cc4042ff85f7d3122b5cd6e40004f73c1931c9bb
svivekan1 <sanjayvivek@hotmail.com>
2018-01-28 12:46:17 +0000

    Config updated

--- config/dogs_vs_cats_config.py
+++ config/dogs_vs_cats_config.py
@@ -1,5 +1,9 @@
+import os
+
 # define the paths to the images directory
-IMAGES_PATH = "../datasets/kaggle_dogs_vs_cats/train"
+# IMAGES_PATH = "../datasets/kaggle_dogs_vs_cats/train"
+BASE_PATH = r"C:\github\\create_hdf5\deeplearning\datasets\kaggle_dogs_vs_cats"
+IMAGES_PATH = os.path.join(BASE_PATH, "train")
 
 # since we do not have validation data or access to the testing
 # labels we need to take a number of images from the training
@@ -9,16 +13,17 @@ NUM_VAL_IMAGES = 1250 * NUM_CLASSES
 NUM_TEST_IMAGES = 1250 * NUM_CLASSES
 
 # define the path to the output training, validation, and testing HDF5 files
-TRAIN_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/train.hdf5"
-VAL_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/val.hdf5"
-TEST_HDF5 = "../datasets/kaggle_dogs_vs_cats/hdf5/test.hdf5"
-
-# path to the output model file
-MODEL_PATH = "output/alexnet_dogs_vs_cats.model"
-
-# define the path to the dataset mean
-DATASET_MEAN = "output/dogs_vs_cats_mean.json"
+HDF5_PATH = r"C:\github\\create_hdf5\deeplearning\datasets\kaggle_dogs_vs_cats\hdf5"
+TRAIN_HDF5 = os.path.join(HDF5_PATH, "train.hdf5")
+VAL_HDF5 = os.path.join(HDF5_PATH, "val.hdf5")
+TEST_HDF5 = os.path.join(HDF5_PATH, "test.hdf5")
 
 # define the path to the output directory used for storing plots,
 # classification reports, etc.
-OUTPUT_PATH = "output"
+OUTPUT_PATH = r"C:\github\create_hdf5\output"
+
+# path to the output model file
+MODEL_PATH = os.path.join(HDF5_PATH, "alexnet_dogs_vs_cats.model")
+
+# define the path to the dataset mean
+DATASET_MEAN = os.path.join(HDF5_PATH, "dogs_vs_cats_mean.json")
